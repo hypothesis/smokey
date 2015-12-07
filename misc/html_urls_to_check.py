f49cb2a4bef0cd0c97eea7d48783fd0a3fbf9ca1
judell <jon@jonudell.info>
2015-12-07 10:56:15 -0800

    initial set of checklists

---
+++ misc/html_urls_to_check.py
@@ -0,0 +1,43 @@
+html_checklist = (
+'https://web.archive.org/web/20150311161739/http://archive.org/stream/MarxismAndLiteraryCriticism/Marxism_and_Literary_Criticism_djvu.txt', # https://github.com/hypothesis/via/issues/63
+'https://www.facebook.com/notes/990860537613477/',
+'https://peerj.github.io/paper-now/',
+'https://medium.com/@jedsundwall/how-do-we-stay-sane-57b4078974fb',
+'https://medium.com/@joshelman/four-questions-to-understanding-adoption-8fc499910e8a', # https://github.com/hypothesis/via/issues/48
+'https://www.facebook.com/WestEndFarmersMarketSantaRosa',
+'http://teaching.lfhanley.net/eng528f15/texts/w-e-b-du-bois-of-our-spiritual-strivings/', # https://github.com/hypothesis/via/issues/39
+'http://www.theawl.com/2015/07/in-no-charts', # https://github.com/hypothesis/via/issues/31
+'http://www.ebi.ac.uk/ena/data/view/SRX000429', # https://github.com/hypothesis/via/issues/56
+'http://w2.vatican.va/content/francesco/en/encyclicals/documents/papa-francesco_20150524_enciclica-laudato-si.html',
+'http://www.newyorker.com/magazine/2015/05/18/tomorrows-advance-man',
+'http://www.nytimes.com/2015/01/17/science/earth/2014-was-hottest-year-on-record-surpassing-2010.html', # https://github.com/hypothesis/h/issues/2712
+'http://www.wsj.com/articles/climate-science-is-not-settled-1411143565', # https://github.com/hypothesis/h/issues/1587
+'http://www.theatlantic.com/technology/archive/2014/08/advertising-is-the-internets-original-sin/376041/',
+'http://blogs.kqed.org/education/2015/04/24/what-is-the-value-of-a-high-school-diploma/', # in via: Uncaught TypeError: jQuery is not a function
+'http://www.perseus.tufts.edu/hopper/text?doc=Perseus:text:1999.04.0104:entry=ajax-bio-1',
+'http://boundary2.org/2015/04/15/a-darked-warped-reflection/#',
+'http://tkbr.ccsp.sfu.ca/pub802/2015/03/publishing-needs-a-hero-just-like-in-the-movies/',
+'http://www.folgerdigitaltexts.org/html/MND.html',
+'http://angular-tips.com/blog/2014/02/introduction-to-unit-test-introduction/',
+'http://annarborobserver.com/articles/mary_morgan_and_dave_askins_full_article.html',
+'http://arstechnica.com/information-technology/2013/08/forget-post-pc-pervasive-computing-and-cloud-will-change-the-nature-of-it/',
+'http://blog.peerlibrary.org/post/60965604596/two-thousand-years-ago-plato-wrote-the-collected#notes', # tests https://github.com/hypothesis/h/issues/1909
+'http://chronicle.com/article/The-MLA-Tells-It-Like-It-Is/146983',
+'http://de.wikipedia.org/wiki/Dialektische_Darstellungsmethode',
+'http://onedio.com/haber/internetin-derin-sulari-sasirtici-yonleri-bilinmeyenleri-ile-deep-web-456498',
+'http://onlinelibrary.wiley.com/doi/10.1002/wdev.107/full',
+'http://stackoverflow.com/questions/21013165/how-to-display-a-div-on-mouse-over-using-jquery',
+'http://techcrunch.com/2014/04/12/culture-eats-strategy-for-breakfast/?ncid=twittersocialshare',
+'http://www.autostraddle.com/the-new-yorkers-skewed-history-of-trans-exclusionary-radical-feminism-ignores-actual-trans-women-247642/',
+'http://www.economist.com/blogs/democracyinamerica/2015/04/mike-huckabee-and-2016',
+'http://www.emule.com/poetry/?page=poem&poem=4329',
+'http://www.infoworld.com/article/2886828/collaboration-software/github-for-the-rest-of-us.html', # 20 second ad
+'http://www.plosone.org/article/info:doi/10.1371/journal.pone.0078188',
+'http://www.youtube.com/watch?v=JQiLaKdzD0E',
+'http://arstechnica.com/gaming/2015/04/valve-software-co-founder-gabe-newell-becomes-a-video-game-character/', # https://github.com/hypothesis/h/issues/2129
+'http://www.georgianjournal.ge/politics/30550-abandon-ship-is-the-national-movement-no-longer-united.html',
+#'http://firstmonday.org/ojs/index.php/fm/article/view/2484/2199', # https://github.com/hypothesis/h/issues/2724
+                                                                   # untestable now because of doc equiv problems
+			                                                       # ideally add to test once that's resolved
+
+)
---
+++ misc/pdf_urls_to_check.py
@@ -0,0 +1,17 @@
+pdf_checklist = (
+'http://www.jceps.com/wp-content/uploads/PDFs/10-2-03.pdf',
+'http://caseyboyle.net/sense/hawhee.pdf',
+'http://blogs.law.harvard.edu/copyrightx/files/2014/01/Understanding-Judicial-Opinions.pdf',
+'http://arxiv.org/pdf/1207.0580v1.pdf',
+'http://cyber.law.harvard.edu/people/tfisher/IP/1991_Feist.pdf',
+'http://faculty.georgetown.edu/irvinem/theory/Berners-Lee-HTTP-proposal.pdf', # https://github.com/hypothesis/h/issues/1647
+'http://partners.adobe.com/public/developer/en/pdf/PDFReference16.pdf',
+'http://users-cs.au.dk/danvy/the-ideal-mathematician.pdf',
+'http://wiki.openoil.net/images/4/43/AR01_07-06-2013_05-05-13_FULL_LIST.pdf',
+'http://www.americanbar.org/content/dam/aba/publications/supreme_court_preview/BriefsV4/13-983_pet_amcu_mbbfap.authcheckdam.pdf',
+'http://faculty.georgetown.edu/irvinem/theory/Berners-Lee-HTTP-proposal.pdf', # https://github.com/hypothesis/h/issues/1647
+'https://nfllabor.files.wordpress.com/2015/05/investigative-and-expert-reports-re-footballs-used-during-afc-championsh.pdf', # https://github.com/hypothesis/h/issues/2476
+'http://www.plosone.org/article/fetchObject.action?uri=info:doi/10.1371/journal.pone.0115253&representation=PDF', # https://github.com/hypothesis/h/issues/1900
+'http://www.openthegovernment.org/sites/default/files/cisa_2015_discussion_draft.pdf', # https://github.com/hypothesis/h/issues/2395
+'http://pubs.usgs.gov/pp/pp1661f/pp1661f_text.pdf', #
+)
